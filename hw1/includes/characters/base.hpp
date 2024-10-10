081b8d99012ab7428b7e08fceec7263ea6ab3732
ilyaki6o_mac <ilgoncharov137@gmail.com>
2024-10-11 00:17:11 +0300

    add base charater class

---
+++ hw1/includes/characters/base.hpp
@@ -0,0 +1,48 @@
+#ifndef BASE
+#define BASE
+
+#include <string>
+#include "../my_shared_ptr/shared_ptr.hpp"
+#include <iostream>
+
+class Player {
+protected:
+    std::string name;
+    bool is_alive;
+
+public:
+    Player(const std::string& name) : name(name), is_alive(true) {}
+
+    virtual std::string performNightAction(std::vector<msp::shared_ptr<Player>>& alivePlayers) {
+        return "virtual func performNightAction";
+    }
+
+    virtual std::string vote(std::vector<msp::shared_ptr<Player>>& alivePlayers) {
+        return "virtual func vote";
+    }
+
+    bool isAlive() const {
+        return is_alive;
+    }
+
+    void kill(){
+        is_alive = false;
+    }
+
+    void setAlive(){
+        is_alive = true;
+    }
+
+    std::string getName() const {
+        return name;
+    }
+
+    virtual ~Player() = default;
+
+    friend std::ostream& operator<<(std::ostream& os, const Player& player) {
+        os << player.name << (player.is_alive ? " (жив)" : " (мертв)");
+        return os;
+    }
+};
+
+#endif
