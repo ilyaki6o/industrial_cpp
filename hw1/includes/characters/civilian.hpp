cc5884a59d1c0dfb71fb712806591d6aa6dbd0b3
ilyaki6o_mac <ilgoncharov137@gmail.com>
2024-10-13 02:46:06 +0300

    seems to end with civilian

--- hw1/includes/characters/civilian.hpp
+++ hw1/includes/characters/civilian.hpp
@@ -2,17 +2,56 @@
 #define CIVILIAN_HPP
 
 #include <string>
-#include <vector>
+#include <map>
 
 #include "base.hpp"
 #include "../my_shared_ptr/shared_ptr.hpp"
+#include <boost/asio.hpp>
 
 class Civilian : public Player {
 public:
-    Civilian(const std::string& name) : Player(name, Role::CIVILIAN) {}
+    Civilian(const std::string& name, PlayerStatus status = PlayerStatus::BOT) : Player(name, Role::CIVILIAN, status) {}
 
-    std::string performNightAction(std::vector<msp::shared_ptr<Player>>& alivePlayers, Logger& logger) override {
-        return "";
+    awaitable<void> vote(
+        std::map<std::string, msp::shared_ptr<Player>>& players,
+        Logger& logger,
+        posix::stream_descriptor& in
+    ) override{
+        if (players.empty()) {
+            std::cerr << "There is no players" << std::endl;
+            co_return;
+        }
+
+        if (this->getStatus() == PlayerStatus::BOT){
+            co_await Player::vote(players, logger, in);
+            co_return;
+        }
+
+        auto mapValues = [](const auto& pair){ return pair.second; };
+        auto chooseAlive = [](const auto& player){ return player->isAlive(); };
+        auto getNames = [](const auto& player){ return player->getName(); };
+
+        auto alivePlayersNames = players | std::views::transform(mapValues) | std::views::filter(chooseAlive) | std::views::transform(getNames);
+
+        std::string chosenName = co_await requestToUser(in, "Enter name of alive player", alivePlayersNames);
+
+        if (chosenName.empty()){
+            co_await Player::vote(players, logger, in);
+            co_return;
+        }
+
+        msp::shared_ptr<Player> chosenPlayer = players[chosenName];
+
+        logger.log(
+            "Игрок " + this->getName()
+            + " (" + this->getStrRole() + ") "
+            + "выбрал игрока" + chosenPlayer->getName()
+            + " (" + chosenPlayer->getStrRole() +") "
+        );
+
+        chosenPlayer->voteUp();
+
+        co_return;
     }
 
     virtual ~Civilian() = default;
