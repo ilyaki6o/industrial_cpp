41ff2f01bc387c14215614e2b4d7f89192bf6207
ilyaki6o_mac <ilgoncharov137@gmail.com>
2024-10-13 15:09:28 +0300

    add vote method to civilian to better class read

--- hw1/includes/characters/civilian.hpp
+++ hw1/includes/characters/civilian.hpp
@@ -12,6 +12,14 @@ class Civilian : public Player {
 public:
     Civilian(const std::string& name, PlayerStatus status = PlayerStatus::BOT) : Player(name, Role::CIVILIAN, status) {}
 
+    virtual awaitable<void> vote(
+        std::map<std::string, msp::shared_ptr<Player>>& players,
+        Logger& logger,
+        posix::stream_descriptor& in
+    ) override {
+        co_await Player::vote(players, logger, in);
+    }
+
     virtual ~Civilian() = default;
 };
 
