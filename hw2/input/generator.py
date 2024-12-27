7cbce2fbccc0a3691d501b3f46fa7c01137f17e7
ilya_goncharov <s02210358@gse.cs.msu.ru>
2024-12-27 16:27:35 +0300

    add generator

--- hw2/input/generator.py
+++ hw2/input/generator.py
@@ -11,3 +11,6 @@ def generator(k, n, lim1, lim2, file_name):
         data = [k]
         data += [rng.randrange(lim1, lim2+1) for _ in range(n)]
         writer.writerow(data)
+
+if __name__ == "__main__":
+    generator(50, 11000, 1, 150, "input/res_11000_50.csv")
